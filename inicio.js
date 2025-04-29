d8e4a785073dfdf27adffacf9f8f69f3e45f49bd
CesarAguilar1203 <cesaraguilar.2302082@gmail.com>
2025-04-29 13:53:59 -0600

    Guardado

--- inicio.js
+++ inicio.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-
-
-
-    
+document.getElementById("esconder").addEventListener("click", function () {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("hidden");
+});
 });
